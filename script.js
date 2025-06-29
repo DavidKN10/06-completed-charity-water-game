277a5627e6c8ae658381d225fb91181d073f04c3
DavidKN10 <davidrs23178@gmail.com>
2025-06-29 16:33:24 -0500

    added home screen and level select

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ startBtn.addEventListener('click', function() {
     // Go to the level select page
     window.location.href = 'level_select.html';
 });
+
+// This function will run when a level is selected
+function selectLevel(level) {
+    // For now, just show an alert with the selected level
+    // You can change this to load the actual level page
+    window.location.href = `levels/level${level}.html`;
+}
