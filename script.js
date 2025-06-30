271fa3a4ea16a59c8b33b903f9acd479bffdae14
DavidKN10 <davidrs23178@gmail.com>
2025-06-29 20:23:31 -0500

    added audio when pressing buttons in the menu

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
  // Get the button element
 const startBtn = document.getElementById('startBtn');
+
+const selectAudio = document.getElementById('selectAudio');
+
 // Add a click event to go to level_select.html
 startBtn.addEventListener('click', function() {
     // Go to the level select page
-    window.location.href = 'level_select.html';
+    //window.location.href = 'level_select.html';
+    selectAudio.currentTime = 0; // Rewind to start
+            selectAudio.play();
+
+            // Wait for the audio to finish, then go to the next page
+            selectAudio.onended = function() {
+                window.location.href = 'level_select.html';
+            };
 });
 
 // This function will run when a level is selected
